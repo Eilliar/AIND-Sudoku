fe20968614291efafd2628219f9ec33958d53d02
Eilliar <lord.eilliar@hotmail.com>
2017-02-19 10:53:28 -0300

    Code to solve a non-diagonal sudoku.

--- solution.py
+++ solution.py
@@ -1,5 +1,15 @@
+###########################################################
+# Globlas
+###########################################################
+# Define rows and columns
+rows = 'ABCDEFGHI'
+cols = '123456789'
+
 assignments = []
 
+###########################################################
+# Functions
+###########################################################
 def assign_value(values, box, value):
     """
     Please use this function to update your values dictionary!
@@ -21,10 +31,13 @@ def naked_twins(values):
 
     # Find all instances of naked twins
     # Eliminate the naked twins as possibilities for their peers
+    pass
 
 def cross(A, B):
-    "Cross product of elements in A and elements in B."
-    pass
+    """
+    Cross('abc', 'def') will return ['ad', 'ae', 'af', 'bd', 'be', 'bf', 'cd', 'ce', 'cf']
+    """
+    return [s+t for s in A for t in B]
 
 def grid_values(grid):
     """
@@ -36,7 +49,10 @@ def grid_values(grid):
             Keys: The boxes, e.g., 'A1'
             Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
     """
-    pass
+    first = dict(zip(boxes, grid))
+    sec = {'.': '123456789'}
+
+    return {k: sec.get(v, v) for k, v in first.items()}
 
 def display(values):
     """
@@ -44,19 +60,81 @@ def display(values):
     Args:
         values(dict): The sudoku in dictionary form
     """
-    pass
+    width = 1+max(len(values[s]) for s in boxes)
+    line = '+'.join(['-'*(width*3)]*3)
+    for r in rows:
+        print(''.join(values[r+c].center(width)+('|' if c in '36' else '') for c in cols))
+        if r in 'CF':
+            print(line)
+    return
 
 def eliminate(values):
-    pass
+    """
+    Go through all the boxes, and whenever there is a box with a single value,
+    eliminate this value from the set of values of all its peers.
+    """
+    
+    # Using Udacity's version...it seems more elegant.
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    for box in solved_values:
+        digit = values[box]
+        for peer in peers[box]:
+            values[peer] = values[peer].replace(digit,'')
+    return values
 
 def only_choice(values):
-    pass
+    """
+    Go through all the units, and whenever there is a unit with a value that only fits in one box, assign the value to this box.
+    Input: A sudoku in dictionary form.
+    Output: The resulting sudoku in dictionary form.
+    """
+    digits = '123456789'
+    for unit in unitlist:
+        for digit in digits:
+            # Get the boxes where a given digit appears on the unit
+            digitPlaces = [box for box in unit if digit in values[box]]
+            if len(digitPlaces) == 1:
+                values = assign_value(values, digitPlaces[0], digit)
+    return values
 
 def reduce_puzzle(values):
-    pass
+    """
+    Iterate eliminate() and only_choice(). If at some point, there is a box with no available values, return False.
+    If the sudoku is solved, return the sudoku.
+    If after an iteration of both functions, the sudoku remains the same, return the sudoku.
+    Input: A sudoku in dictionary form.
+    Output: The resulting sudoku in dictionary form.
+    """
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    stalled = False
+    while not stalled:
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+        values = eliminate(values)
+        values = only_choice(values)
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        stalled = solved_values_before == solved_values_after
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
 
 def search(values):
-    pass
+    """
+    Using depth-first search and propagation, create a search tree and solve the sudoku.
+    """
+    values = reduce_puzzle(values)
+    if values == False:
+        return False
+    if all(len(values[s]) == 1 for s in boxes):
+        return values
+    # Choose one of the unfilled squares with the fewest possibilities
+    a, b = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
+    # Now use recursion to solve each one of the resulting sudokus, and if one returns a value (not False), return that answer!
+    for v in values[b]:
+        n_sudoku = values.copy()
+        n_sudoku[b] = v
+        n_solution = search(n_sudoku)
+        if n_solution:
+            return n_solution
 
 def solve(grid):
     """
@@ -67,16 +145,34 @@ def solve(grid):
     Returns:
         The dictionary representation of the final sudoku grid. False if no solution exists.
     """
+    # Convert to Dictionary
+    values = grid_values(grid)
+    # Doubt: What if search returns False, can display() handle it?
+    return search(values)
 
+###########################################################
+# Problem Representation
+###########################################################
+boxes = cross(rows, cols)
+row_units = [cross(r, cols) for r in rows]
+column_units = [cross(rows, c) for c in cols]
+square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+unitlist = row_units + column_units + square_units
+units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
+
+###########################################################
+# Run Script
+###########################################################
 if __name__ == '__main__':
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
     display(solve(diag_sudoku_grid))
 
-    try:
-        from visualize import visualize_assignments
-        visualize_assignments(assignments)
+    # try:
+    #     from visualize import visualize_assignments
+    #     visualize_assignments(assignments)
 
-    except SystemExit:
-        pass
-    except:
-        print('We could not visualize your board due to a pygame issue. Not a problem! It is not a requirement.')
+    # except SystemExit:
+    #     pass
+    # except:
+    #     print('We could not visualize your board due to a pygame issue. Not a problem! It is not a requirement.')
